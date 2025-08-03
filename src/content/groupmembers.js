52bb606204f5cbf95c33755a4f0749c95e4ade48
anshulyadaviitk <anshulyadav.iitk@gmail.com>
2025-08-03 21:33:16 +0530

    Update groupmembers.js

--- src/content/groupmembers.js
+++ src/content/groupmembers.js
@@ -266,7 +266,7 @@ images:[
     id: 'phd-04',
     type: 'PhD',
     name: 'Ms. Suman Supriya Sahu',
-    photo: '/images/groupmembers/intern/suman.jpeg',
+    photo: '/images/groupmembers/intern/suman.jpg',
     position: 'PhD Scholar',
     peerReviewedPublications: [
   'N/A'
