7c1ae3d09e3342c9c1260d935e2b3309be7a8cb1
anshulyadaviitk <anshulyadav.iitk@gmail.com>
2025-09-29 07:42:28 +0530

    Update collaborations.js

--- src/content/collaborations.js
+++ src/content/collaborations.js
@@ -49,6 +49,18 @@ export const National = [
   }
 ];
 
+export const Industry = [
+  {
+    name: "Cloudastra",
+    logo: "/images/collaborations/logo_international/czestochowa_poland.jpg",
+  },
+
+    {
+    name: "VP Eco Trade LLP",
+    logo: "/images/collaborations/logo_international/czestochowa_poland.jpg",
+  },
+];
+
 export const International = [
   {
     name: "Czestochowa University of Technology, Poland",
