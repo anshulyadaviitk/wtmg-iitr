bc291a6d518b477f77df21c40a0f99a85b11f06f
anshulyadaviitk <anshulyadav.iitk@gmail.com>
2025-11-17 10:24:36 +0530

    Update groupmembers.js

--- src/content/groupmembers.js
+++ src/content/groupmembers.js
@@ -171,43 +171,7 @@ export const pi = {
 };
 
 export const postdoc = [
-  {
-    id: 'postdoc-01',
-    type: 'PhD',
-    name: 'Mr. Nikhil',
-    photo: '/images/groupmembers/phd/nikhil.jpeg',
-    position: 'Postdoctoral Researcher',
-    peerReviewedPublications: [
  
-],
-patents: [
-  'N/A'
-],
-conferences: [
-  'N/A'
-],
-awards: [
-  'N/A'
-]
-,
-images:[
-  '/images/events/event1.jpg',
-    '/images/events/event2.jpg',
-    '/images/events/event3.jpg',
-    '/images/events/event4.jpg'
-],
-   previousDegree: "Master of Technology from National Institute of Technology Jalandhar (2020)\nBachelor of Technology from Uttar Pradesh Technical University (2016)",
-    topic: 'Development of Membrane Materials for Enhanced Electrodialysis Performance in Water and Wastewater Treatment',
-    duration: 'January 2025 – Present',
-    email: '',
-    contact: '',
-    coSupervisor: 'Dr. Prasanta K. Sahoo, National Institute of Hydrology, Roorkee',
-    links: {
-      linkedin: 'XXXX',
-      researchgate: 'XXXX',
-      scholar: 'XXXX'
-    }
-  },
 ];
 
 export const phdScholars = [
@@ -607,42 +571,7 @@ export const mastersStudents = [
 ];
 
 export const project_student = [
-   {
-    id: 'project-01',
-    type: 'Research Assistant',
-    name: 'Ms. Suman Supriya Sahu',
-    photo: '/images/groupmembers/interns/suman.jpg',
-    position: 'Research Assistant',
-    peerReviewedPublications: [
-  'N/A'
-],
-patents: [
-  'N/A'
-],
-conferences: [
-  'N/A'
-],
-awards: [
-  'UGC NET JRF 2025'
-]
-,
-images:[
-  '/images/events/event1.jpg',
-    '/images/events/event2.jpg',
-    '/images/events/event3.jpg',
-    '/images/events/event4.jpg'
-],
-    previousDegree: "Integrated MSc in Environmental Science from Central University of Rajasthan (2024)",
-    topic: 'Design and Fabrication of Capacitive Deionization system for wastewater treatment.',
-    duration: "August 2025 – November 2025",
-    email: 'sumansupriya274@gmail.com',
-    contact: 'XXXX',
-    links: {
-      linkedin: 'XXXX',
-      researchgate: 'XXXX',
-      scholar: 'XXXX'
-    }
-  }
+  
 ];
 
 export const interns = [
